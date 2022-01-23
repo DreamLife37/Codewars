7bc0ddabf348dc514cf1437354f2092f48de177a
Andrey <fail4iks@gmail.com>
2022-01-23 18:13:47 +0300

    Task 11: Grasshopper - Grade book

--- Codewars.js
+++ Codewars.js
@@ -127,4 +127,28 @@ const quarterOf = (month) => {
 }
 
 //либо
-const quarterOf = m => Math.ceil(m/3);
+const quarterOf = m => Math.ceil(m / 3);
+
+//Task 11: Grasshopper - Grade book
+/* Найти среднее значение и вернуть букву
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F' */
+
+function getGrade(s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3
+  if (score >= 90) {  return 'A'
+  } else if ( score >= 80) {
+    return 'B'
+  } else  if (score >= 70) {
+    return 'C'
+  } else  if (score >= 60) {
+    return 'D'
+  } else  if (score < 60) {
+    return 'F'
+  }
+}
+
+console.log(getGrade(70, 72, 78))
